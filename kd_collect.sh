293235a5389362361720a5ab79f3242328a728a1
Kirill Davydychev <kdavyd@gmail.com>
2013-02-06 20:55:10 -0600

    Update kd_collect.sh

--- kd_collect.sh
+++ kd_collect.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/bash
-#
-# Author: Kirill.Davydychev@Nexenta.com
-# Copyright 2013, Nexenta Systems, Inc. 
-#
-
-un="/usr/bin/uname"
-
-if [[ ! $(${un} -v) =~ NexentaOS ]]; then
-  printf "%s\n" "[CRITICAL] System is not a NexentaStor Appliance."
-	exit 1
-
 zfs create syspool/perflogs
 zfs set compression=gzip-9 syspool/perflogs
 zfs set mountpoint=/perflogs syspool/perflogs
-
 cd /perflogs
 wget https://raw.github.com/kdavyd/dtrace/master/nfsutil.d --no-ch
 wget https://raw.github.com/kdavyd/dtrace/master/txg_monitor.v3.d --no-ch
@@ -28,4 +16,3 @@ nmc -c "show performance arc" >> arcstat.out &
 while true; do date >> arc.out; echo ::arc | mdb -k >> arc.out; sleep 60; done &
 echo "The logging is now set up. It will run indefinitely until the system is rebooted."
 echo "Please collect logs from the /perflogs/ folder in the root of the appliance."
-exit 0
