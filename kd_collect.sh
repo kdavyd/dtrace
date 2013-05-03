0f3aa45c8e5572f529b8c7f0b1cd8d3103ea3858
Kirill Davydychev <kdavyd@gmail.com>
2013-05-03 15:50:08 -0500

    added date to arcstat

--- kd_collect.sh
+++ kd_collect.sh
@@ -50,7 +50,7 @@ for i in `zpool list -H -o name`; do
   ./txg_monitor.v3.d $i >> txg.$i.out &
 done
 ./kmem_reap_100ms.d >> kmem.out &
-./arcstat.pl -f read,hits,miss,hit%,l2read,l2hits,l2miss,l2hit%,arcsz,l2size 1 >> arcstat.out &
+./arcstat.pl -f date,read,hits,miss,hit%,l2read,l2hits,l2miss,l2hit%,arcsz,l2size 1 >> arcstat.out &
 ./zfsio.d >> zfsio.out &
 while true; do date >> arc.out; echo ::arc | mdb -k >> arc.out; sleep 60; done &
 sleep 5
