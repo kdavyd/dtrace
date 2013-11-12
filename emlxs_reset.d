d4518c127b508aa03fc24e52a8b710ee3976699c
Kirill Davydychev <kdavyd@gmail.com>
2013-11-11 20:26:44 -0600

    replaced arg1 with args[1]

--- emlxs_reset.d
+++ emlxs_reset.d
@@ -4,12 +4,12 @@
 emlxs_reset:entry
 {
     @[stack(100)]=count();
-    printf("%Y: Reset entry command %d\n",arg1);
+    printf("%Y: Reset entry command %d\n",args[1]);
 }
 
 emlxs_reset:return
 {
-    printf("%Y: Reset return code %d\n",walltimestamp,arg1);
+    printf("%Y: Reset return code %d\n",walltimestamp,args[1]);
     printa(@);
 }
 
