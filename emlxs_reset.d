5cd6ab23edeb59751103269cc5d933e6176bc027
Kirill Davydychev <kdavyd@gmail.com>
2013-11-11 19:28:54 -0600

    Create emlxs_reset.d
    Script to trace Emulex HBA resets to find their root cause.

---
+++ emlxs_reset.d
@@ -0,0 +1,19 @@
+#!/usr/sbin/dtrace
+#pragma D option quiet
+
+emlxs_reset:entry
+{
+    @[stack(100)]=count();
+    printf("%Y: Reset entry command %d\n",arg0);
+}
+
+emlxs_reset:return
+{
+    printf("%Y: Reset return code %d\n",walltimestamp,arg1);
+    printa(@);
+}
+
+tick-30sec
+{
+    printa(@);trunc(@);
+}
