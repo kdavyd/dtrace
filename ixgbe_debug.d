355264257a577cb06c4b55e361a7d1b4f06fec95
Kirill Davydychev <kdavyd@gmail.com>
2014-04-18 16:29:42 -0500

    Update ixgbe_debug.d

--- ixgbe_debug.d
+++ ixgbe_debug.d
@@ -4,23 +4,23 @@
 ixgbe_identify_sfp_module_generic:return
 /arg1==-19/
 {
-    printf("%s:%s\n",probefunc,"IXGBE_ERR_SFP_NOT_SUPPORTED")};
+    printf("%s:%s\n",probefunc,"IXGBE_ERR_SFP_NOT_SUPPORTED");
 }
 
 ixgbe_identify_sfp_module_generic:return
 /arg1==-20/
 {
-    printf("%s:%s\n",probefunc,"IXGBE_ERR_SFP_NOT_PRESENT")};
+    printf("%s:%s\n",probefunc,"IXGBE_ERR_SFP_NOT_PRESENT");
 }
 
 ixgbe_identify_sfp_module_generic:return
 /arg1==-21/
 {
-    printf("%s:%s\n",probefunc,"IXGBE_ERR_SFP_NO_INIT_SEQ_PRESENT")};
+    printf("%s:%s\n",probefunc,"IXGBE_ERR_SFP_NO_INIT_SEQ_PRESENT");
 }
 
 ixgbe_identify_sfp_module_generic:return
 /arg1==0/
 {
-    printf("%s:%s\n",probefunc,"IXGBE_SUCCESS")};
+    printf("%s:%s\n",probefunc,"IXGBE_SUCCESS");
 }
