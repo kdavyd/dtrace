dc7a0c4b91ea49c87e43a81444ec1d4a846c9dc7
Kirill Davydychev <kdavyd@gmail.com>
2014-08-16 13:43:35 -0500

    Cleanup

--- parse_zfsio.py
+++ parse_zfsio.py
@@ -4,11 +4,7 @@ import pprint
 from datetime import datetime,date,time
 
 def main():
-    print str(sys.argv)
-    input = sys.argv[1]
-    i=0
-    with open(input) as f:
-        dl={}
+    with open(sys.argv[1]) as f:
         dt=""
         while True:
             line = f.readline()
@@ -27,7 +23,6 @@ def is_timestamp(t):
         return False
 
 def dp_convert_and_write(t,t_date):
-    dp = {}
     t = t.split()
     f = open (t[0].replace("/","_")+".zfsio.csv", "a")
     f.write(datetime.strftime(t_date,'%Y-%m-%d-%T')+','+t[1]+','+t[2]+','+t[3]+','+t[4]+','+t[5]+','+t[6]+'\n')
