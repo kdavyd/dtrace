576fcbbcd1b50f81b04f8af4df0c1069a918f27d
Kirill Davydychev <kdavyd@gmail.com>
2015-04-06 18:54:38 -0500

    Added timestamp

--- smb_trace.d
+++ smb_trace.d
@@ -5,5 +5,5 @@ sdt:smbsrv::-smb_op-NtCreateX-start
 {
         sr =  (struct smb_request *)arg0;
         op =  (struct open_param *)arg1;
-        printf("%s %s %s\n", stringof(sr->uid_user->u_name), stringof(sr->tid_tree->t_resource), stringof(op->fqi.fq_path.pn_path));
+        printf("%Y %s %s %s\n", walltimestamp, stringof(sr->uid_user->u_name), stringof(sr->tid_tree->t_resource), stringof(op->fqi.fq_path.pn_path));
 }
