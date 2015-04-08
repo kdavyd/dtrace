413769fa9fc6b715f7dad078e241c5fb3c95346b
Kirill Davydychev <kdavyd@gmail.com>
2015-04-08 14:17:04 -0500

    Changed scalar variables to thread-local

--- smb_trace.d
+++ smb_trace.d
@@ -3,7 +3,7 @@
 
 sdt:smbsrv::-smb_op-NtCreateX-start
 {
-        sr =  (struct smb_request *)arg0;
-        op =  (struct open_param *)arg1;
-        printf("%Y %s %s %s\n", walltimestamp, stringof(sr->uid_user->u_name), stringof(sr->tid_tree->t_resource), stringof(op->fqi.fq_path.pn_path));
+        self->sr =  (struct smb_request *)arg0;
+        self->op =  (struct open_param *)arg1;
+        printf("%Y %s %s %s\n", walltimestamp, stringof(self->sr->uid_user->u_name), stringof(self->sr->tid_tree->t_resource), stringof(self->op->fqi.fq_path.pn_path));
 }
